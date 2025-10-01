593bb16d472cde601a7fb0b3c250589a245d36c1
David Hernandez <davidhernandz05@gmail.com>
2025-10-01 18:00:11 -0500

    Agrego 2 ejercicios muy basicos en python.

--- Python/Proyectos/Python/Curso/Modulos/main.py
+++ Python/Proyectos/Python/Curso/Modulos/main.py
@@ -1,3 +1,12 @@
-import miModulo
+# Importa todo pero al momento de llamar una función tenemos que poner mi modulo.
+#import miModulo
 
-print(miModulo.holamundo("David Hernandez"))
+#Importar solamente un afunción:
+#from miModulo import holamundo
+
+#Importamos todas las funciones de miModulo pero ya no tenemos que llamar al modulo.
+from miModulo import *
+
+#print(miModulo.holamundo("David Hernandez"))
+#print(holamundo("Miguel"))
+print(holamundo("David"))
