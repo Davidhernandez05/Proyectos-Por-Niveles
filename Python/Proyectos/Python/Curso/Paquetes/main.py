ddc81521571a2cac4126ea1b1fc14692c7c8e7e7
David Hernandez <davidhernandz05@gmail.com>
2025-10-06 10:53:11 -0500

    Actualizo el archivo de paquetes.

--- Python/Proyectos/Python/Curso/Paquetes/main.py
+++ Python/Proyectos/Python/Curso/Paquetes/main.py
@@ -2,6 +2,8 @@
 from miPaquete import pruebas
 from miPaquete import herramientas
 
+#from miPaquete import herramientas, pruebas -> Esto tambien funciona por que estan en la misma carpeta(Paquete).
+
 print("probando paquetes: ")
 
 herramientas.nombreCompleto("david", "Hernandez")
