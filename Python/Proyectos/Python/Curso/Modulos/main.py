6b45fb2dc7d8081970146aba361c4fd790271fe5
David Hernandez <davidhernandz05@gmail.com>
2025-10-06 10:41:34 -0500

    Agrego 2 ejercicios muy basicos en python.

--- Python/Proyectos/Python/Curso/Modulos/main.py
+++ Python/Proyectos/Python/Curso/Modulos/main.py
@@ -24,3 +24,16 @@ print(fecha_personalizada)
 
 print(datetime.datetime.now().timestamp())
 print(datetime.datetime.now().time())
+
+
+#Modulo de matematicas:
+import math
+
+print("Raiz cuadrada de 10: ", math.sqrt(10))
+print("Numero pi: ", float(math.pi))
+print("Redondear: ", math.ceil(6.12894123846)) # Redondea hacia arriba
+print("Redondear: ", math.floor(6.12894123846)) # Redondea hacia abajo
+
+# Modulo random:
+import random
+print("Numero aleatorio entre 15 y 67: ", random.randint(15, 67))
