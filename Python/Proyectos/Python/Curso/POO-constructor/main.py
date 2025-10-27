0e3338ecaef08b6e708fdf4f3af40634e640cde0
David Hernandez <davidhernandz05@gmail.com>
2025-10-27 01:47:17 -0500

    validar que un objeto sea de un tipo en python.

--- Python/Proyectos/Python/Curso/POO-constructor/main.py
+++ Python/Proyectos/Python/Curso/POO-constructor/main.py
@@ -2,3 +2,12 @@ from coche import Coche
 
 carro = Coche("Porche", "911", "Rojo", 4)
 print(carro.coche())
+
+
+# Detectar tipado:
+
+if type(carro) == Coche:
+  print("Es un objeto de tipo coche.")
+
+else:
+  print("No es un objero de tipo coche.")
