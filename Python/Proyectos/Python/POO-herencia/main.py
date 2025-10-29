5042d54be0626b4975c667bd812ddd18aed848ef
David Hernandez <davidhernandz05@gmail.com>
2025-10-28 19:13:49 -0500

    Actualizo contenido de python -> Herencia.

--- Python/Proyectos/Python/POO-herencia/main.py
+++ Python/Proyectos/Python/POO-herencia/main.py
@@ -9,3 +9,13 @@ persona.setAltura(1.75)
 
 print(f"Bienvenido: {persona.getNombre()} {persona.getApellidos()}")
 print(persona.dormir())
+
+print("-------------------------------")
+
+informatio = clases.Informatico()
+
+informatio.setNombre("Andres")
+informatio.setApellidos("Martinez")
+
+print(f"El informatico es: {informatio.getNombre()} {informatio.getApellidos()}.")
+print(informatio.getLenguajes())
